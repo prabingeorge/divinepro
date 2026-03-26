7f2857adf9447ac05d748df131740d7d6e6e7bd2
prabingeorge <prabingeorge@gmail.com>
2026-03-26 10:43:51 +0530

    Initial code

---
+++ src/assets/assets.js
@@ -0,0 +1,90 @@
+import logo from './divinepro_logo.png'
+import arrow_icon from './arrow_icon.svg'
+import group_profile from './group_profile.png'
+import bgImage1 from './bgImage1.png'
+import bgImage2 from './bgImage2.png'
+import hero_img from './hero_img.png'
+import delightserve_light_logo from './delightserve_light_logo.png'
+import jonbakes_logo from './jonbakes_logo.png'
+import pepconstruction_logo from './pepconstruction_logo.png'
+import coinbase_logo from './coinbase_logo.png'
+import ads_icon from './ads_icon.svg'
+import content_icon from './content_icon.svg'
+import marketing_icon from './marketing_icon.svg'
+import social_icon from './social_icon.svg'
+import menu_icon from './menu_icon.svg'
+import close_icon from './close_icon.svg'
+import work_mobile_app from './work_mobile_app.png'
+import work_mobile_app1 from './work_mobile_app1.png'
+import work_fitness_app from './work_fitness_app.png'
+import work_fitness_app1 from './work_fitness_app1.png'
+import work_dashboard_management from './work_dashboard_management.png'
+import work_dashboard_management1 from './work_dashboard_management1.png'
+import email_icon from './email_icon.svg'
+import person_icon from './person_icon.svg'
+import facebook_icon from './facebook_icon.svg'
+import twitter_icon from './twitter_icon.svg'
+import instagram_icon from './instagram_icon.svg'
+import linkedin_icon from './linkedin_icon.svg'
+import logo_dark from './divinepro_dark_logo.png'
+import airbnb_logo from './airbnb_logo.svg'
+import google_logo from './google_logo.svg'
+import menu_icon_dark from './menu_icon_dark.svg'
+import sun_icon from './sun_icon.svg'
+import moon_icon from './moon_icon.svg'
+
+import men_icon from './men.jpg'
+import women_icon from './women.jpg'
+
+export const company_logos = [
+  delightserve_light_logo,
+  jonbakes_logo,
+  pepconstruction_logo
+]
+
+const assets = {
+  logo,
+  arrow_icon,
+  group_profile,
+  bgImage1,
+  bgImage2,
+  hero_img,
+  ads_icon,
+  content_icon,
+  marketing_icon,
+  social_icon,
+  menu_icon,
+  close_icon,
+  work_mobile_app,
+  work_mobile_app1,
+  work_fitness_app,
+  work_fitness_app1,
+  work_dashboard_management,
+  work_dashboard_management1,
+  email_icon,
+  person_icon,
+  facebook_icon,
+  twitter_icon,
+  instagram_icon,
+  linkedin_icon,
+  logo_dark,
+  menu_icon_dark,
+  sun_icon,
+  moon_icon
+}
+
+export default assets
+
+export const teamData = [
+  { name: 'Abisheck Paul', title: '', image: men_icon },
+  { name: 'Dalton Nelson', title: '', image: men_icon },
+  { name: 'Dnyanesh Pratik', title: '', image: women_icon },
+  { name: 'Jobin Xavier', title: '', image: men_icon },
+  { name: 'Libin George', title: '', image: men_icon },
+  { name: 'Mergin Bright', title: '', image: men_icon },
+  { name: 'Murugan Thomas', title: '', image: men_icon },
+  { name: 'Prabha Stalin', title: '', image: women_icon },
+  { name: 'Prabin George', title: '', image: men_icon },
+  { name: 'Pradeep Micheal', title: '', image: men_icon },
+  { name: 'Sobiya Prabin', title: '', image: women_icon },
+]
